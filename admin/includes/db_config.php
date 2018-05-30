2d9fbdf098bcc927fa0bf1861110b371de4c9f07
hkrzysztof <33382082+hkrzysztof@users.noreply.github.com>
2018-05-30 14:22:24 +0200

    Update db_config.php

--- admin/includes/db_config.php
+++ admin/includes/db_config.php
@@ -1,10 +1,10 @@
 <?php
 
 //DB Connection constants
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'php-cms');
+define('DB_HOST', 'FILL IN');
+define('DB_USER', 'FILL IN');
+define('DB_PASS', 'FILL IN');
+define('DB_NAME', 'FILL IN');
 
 //making connection
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
